046026f38ce2aeb104ae52b3b73e54c8f1e6090a
BobIllumine <38183075+BobIllumine@users.noreply.github.com>
2019-07-05 19:33:57 +0500

    Add files via upload

---
+++ EpicTestBot/Models/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EpicTestBot.Models
+{
+    public static class AppSettings
+    {
+        public static string Url { get; set; } = "https://:443/{0}";
+        public static string Name { get; set; } = "EpicTestBot";
+        public static string Key { get; set; } = "765436647:AAEUXWWrO58h0Qp1BQl2E8sIjiOFwfQc3fs";
+    }
+}
